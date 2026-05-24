47334bb614023ff66764bcf28e635f8998bb1697
Виктория Ахрамович <vikahramovich02@gmail.com>
2026-05-25 00:29:53 +0300

    debug: try-catch around createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+try {
+  ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+} catch(e) {
+  document.getElementById('root').innerHTML = '<pre style="padding:30px;color:red;white-space:pre-wrap;background:#fff"><b>STARTUP ERROR:</b>\n' + (e?.stack || String(e)) + '</pre>';
+}
